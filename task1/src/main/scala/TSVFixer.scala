55932a9ced97d13538d91c98ab99bb3078807bde
Pavel <popov.pa@yandex.ru>
2018-06-06 00:35:01 +0300

    Task

--- task1/src/main/scala/TSVFixer.scala
+++ task1/src/main/scala/TSVFixer.scala
@@ -1,7 +1,23 @@
+import scala.StringContext
+import scala.io.Source
+
 object TSVFixer extends App {
 
+  var correctList:Array[String] = Array[String]()
 
-  print("okay")
+  Source.fromFile("data.tsv", "UTF-16LE").getLines().drop(1).foreach { line =>
+    val tokens = line.split('\t')
+    if (tokens.length == 5) {
+      correctList :+= tokens.mkString(",")
+    }
+  }
+  println(correctList)
+
+  def escape(string:String):String={
 
 
+
+  }
 }
+
+case class Item(id: String, first_name: String, last_name: String, account_number: Long, email: String)
